14aadabfc42605089fb3244b98622bbdd3d52a7a
Raul Morales <irmoralesb@hotmail.com>
2025-07-27 09:13:10 -0600

    Routers

---
+++ src/Detail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Detail() {
+  return <h1>Detail</h1>;
+
+  //   return (
+  //     <div id="detail">
+  //       <h1>{product.name}</h1>
+  //       <p>{product.description}</p>
+  //       <p id="price">${product.price}</p>
+  //       <img src={`/images/${product.image}`} alt="{product.name" />
+  //     </div>
+  //  );
+}
