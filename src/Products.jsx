bdd0d3ecd62ab2ca284abfc25bde3c525291b916
Raul Morales <irmoralesb@hotmail.com>
2025-07-31 22:14:01 -0600

    Cart Context Manager

--- src/Products.jsx
+++ src/Products.jsx
@@ -9,11 +9,9 @@ export default function Products() {
   const [size, setSize] = useState("");
   const { category } = useParams();
 
-  const {
-    data: products,
-    loading,
-    error,
-  } = useFetch("products?category=" + category);
+  const { data: products, loading, error } = useFetch(
+    "products?category=" + category
+  );
 
   function renderProduct(p) {
     return (
